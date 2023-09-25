5069d049b5c11429a77a1260446752aef94695b4
Saekyul <saekyulyang@gmail.com>
2023-09-25 22:12:06 +0900

    글꼴 굵기 변경

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,6 +4,7 @@ export const Title = styled.h1`
   display: flex;
   justify-content: center;
   font-size: 3rem;
+  font-weight: 900;
   text-decoration: underline;
   text-underline-offset: 20px;
   padding: 30px;

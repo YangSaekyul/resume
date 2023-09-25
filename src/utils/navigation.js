bd16a1813621f75a60d87cdae5a9bd15e0d334c1
Saekyul <saekyulyang@gmail.com>
2023-09-25 17:30:37 +0900

    네비게이션 시 어느정도 간격을 두도록 수정

--- src/utils/navigation.js
+++ src/utils/navigation.js
@@ -1,4 +1,9 @@
 export const scrollToSection = (section) => {
   const element = document.getElementById(section);
-  element.scrollIntoView({ behavior: "smooth" });
+  const headerHeight = 100;
+
+  const yOffset = -headerHeight;
+  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+  window.scrollTo({ top: y, behavior: "smooth" });
 };

ee81b7dc4013c04d666395163fb78d2a558c25e7
Saekyul <saekyulyang@gmail.com>
2023-09-26 23:27:18 +0900

    js -> tsx 변경

--- src/utils/navigation.tsx
+++ src/utils/navigation.tsx
@@ -1,7 +1,8 @@
-export const scrollToSection = (section) => {
+export const scrollToSection = (section: string): void => {
   const element = document.getElementById(section);
   const headerHeight = 80;
 
+  if (!element) return;
   const yOffset = -headerHeight;
   const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
